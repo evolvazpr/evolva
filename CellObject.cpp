eda4f8a0a84b6e0b7761ea16a0c481ef40d837cf
Konrad Gotfryd <gotfrydkonrad@gmail.com>
2016-03-06 15:45:45 +0100

    temporary commit

---
+++ CellObject.cpp
@@ -0,0 +1,3 @@
+#include "CellObject.hpp"
+
+RealCoordinates::~RealCoordinates(){}
--- CellObject.hpp
+++ CellObject.hpp
@@ -3,35 +3,41 @@
 
 #include <iostream>
 
-/*
- * Abstract base class for objects.
- * bool isMovable() - needs to be implemented in inheriting classes.
- */
 
-class CellObject {
-	public:
-		CellObject(){};
-		virtual bool is_movable() = 0;
-		virtual ~CellObject(){}
-};
-
-class Plant : public CellObject {
+class RealCoordinates {
 	private:
-		const int energy_;
-		const bool toxic_;
-
+	
+		int x_;
+		int y_;
+	
 	public:
-		Plant(int energy, bool toxic) : CellObject(), energy_(energy), toxic_(toxic) {}
-		bool is_toxic() { return toxic_; }
-		int get_energy_value() { return energy_; }
-		virtual bool is_movable() { return false; }
-		virtual ~Plant() {}
+		
+		RealCoordinates(int x, int y) : x_ (x), y_(y) {}
+		virtual ~RealCoordinates() = 0;
+		int get_real_x() { return x_; }
+		int get_real_y() { return y_; }
+		void get_real_coordinates(int *x, int *y) { *x = x_; *y = y_; }
+
+
+		virtual void set_real_x(int x) { x_ = x; }
+		virtual void set_real_y(int y) { y_ = y; }
+		virtual void set_real_coordinates(int x, int y) { x_ = x; y_ = y; };
 };
 
-class TestUnit : public CellObject {
+class CellObject : public RealCoordinates {
 	public:
-		TestUnit() : CellObject() {}
-		virtual bool is_movable() { return true; }
+		CellObject(int x, int y) : RealCoordinates(x, y) {};
+		virtual bool is_movable() = 0;
+		virtual ~CellObject(){};	
 };
+
+class MovableObject : public CellObject {
+
+};
+
+class NonMovableObject : public CellObject {
+
+};
+
 
 #endif //_CELLOBJECT_H_
