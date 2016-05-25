5760d7b6f1d8dde719a0915aaa4c000fd3c409fd
Konrad Gotfryd <gotfrydkonrad@gmail.com>
2016-05-25 22:31:43 +0200

    changed script - caution - water.png fails to load, i dont know why

--- scripts/generate_gui_xml.sh
+++ scripts/generate_gui_xml.sh
@@ -1,97 +1,44 @@
 #!/bin/bash
-set -e
+
+function create_node() {
+	FILE=`printf "%s\n%s" "$FILE" "<$1>"`
+	FILE=`printf "%s\n%s" "$FILE" "<path>"`
+	FILE=`printf "%s\n%s%s" "$FILE" "$PATH" "/$1.png"`
+	FILE=`printf "%s\n%s" "$FILE" "</path>"`
+	FILE=`printf "%s\n%s" "$FILE" "<sprite_cnt>"`
+	FILE=`printf "%s\n%s" "$FILE" "$2"`
+	FILE=`printf "%s\n%s" "$FILE" "</sprite_cnt>"`
+	FILE=`printf "%s\n%s\n \n" "$FILE" "</$1>"`
+}
+
 mkdir sprites
 cd sprites
-wget -O deer_all_directions.png		http://www.guelnika.net/images/charset/noel/reindeer_eva2.png
-wget -O tiger_all_directions.png	http://eragon-forum.3dn.ru/_fr/6/0245600.png
-wget -O grass.png	http://cdn.photonesta.com/images/4.bp.blogspot.com/-ggkSFkq3ky4/UXO1HwBEtZI/AAAAAAAAM9M/JuxFjeDrJwk/s1600/Sprite_FX_Grass_0001.png
-wget -O sand.png	 www.wurmpedia.com/images/a/a7/Sand.png
-wget -O soil.png	http://www.buildbase.co.uk/img/group/Q/soil.png
-wget -O stone.png	http://www.autoglas-specialist.nl/img/stone.png
-wget -O water.png	http://www.sharecg.com/images/medium/3020.jpg
-wget -O tree.png	http://vignette2.wikia.nocookie.net/herebemonsters/images/4/40/Mango-Tree-Sprite.png/revision/latest?cb=20140328231536
-wget -O unit.png	http://www.serwo.pl/images/promo1.png
+wget -O herbivore.png http://www.guelnika.net/images/charset/noel/reindeer_eva2.png
+wget -O carnivore.png http://eragon-forum.3dn.ru/_fr/6/0245600.png
+wget -O grass.png http://cdn.photonesta.com/images/4.bp.blogspot.com/-ggkSFkq3ky4/UXO1HwBEtZI/AAAAAAAAM9M/JuxFjeDrJwk/s1600/Sprite_FX_Grass_0001.png
+wget -O sand.png www.wurmpedia.com/images/a/a7/Sand.png
+wget -O soil.png http://www.buildbase.co.uk/img/group/Q/soil.png
+wget -O stone.png http://www.autoglas-specialist.nl/img/stone.png
+wget -O water.png http://www.sharecg.com/images/medium/3020.jpg
+wget -O tree.png http://vignette2.wikia.nocookie.net/herebemonsters/images/4/40/Mango-Tree-Sprite.png/revision/latest?cb=20140328231536
+wget -O unit.png http://www.serwo.pl/images/promo1.png
 
 PATH=`pwd`
 
 FILE="<evolva>"
 
-#grass
-FILE+="<grass>"
-FILE+="<path>"
-FILE+=`echo "$PATH"`
-FILE+="/grass.png"
-FILE+="</path>"
-FILE+=`echo`
-FILE+="<sprite_cnt>1</sprite_cnt>"
-FILE+="</grass>"
-FILE+=`echo`
-#water
-FILE+="<water>"
-FILE+="<path>"
-FILE+=`echo "$PATH"`
-FILE+="/water.png"
-FILE+="</path>"
-FILE+="<sprite_cnt>1</sprite_cnt>"
-FILE+="</water>"
-
-#sand
-FILE+="<sand>"
-FILE+="<path>"
-FILE+=`echo "$PATH"`
-FILE+="/sand.png"
-FILE+="</path>"
-FILE+="<sprite_cnt>1</sprite_cnt>"
-FILE+="</sand>"
-
-#soil
-FILE+="<soil>"
-FILE+="<path>"
-FILE+=`echo "$PATH"`
-FILE+="/soil.png"
-FILE+="</path>"
-FILE+="<sprite_cnt>1</sprite_cnt>"
-FILE+="</soil>"
-
-
-#tree
-FILE+="<tree>"
-FILE+="<path>"
-FILE+=`echo "$PATH"`
-FILE+="/tree.png"
-FILE+="</path>"
-FILE+="<sprite_cnt>1</sprite_cnt>"
-FILE+="</tree>"
-
-#herbivore
-FILE+="<herbivore>"
-FILE+="<path>"
-FILE+=`echo "$PATH"`
-FILE+="/deer_all_directions.png"
-FILE+="</path>"
-FILE+="<sprite_cnt>4</sprite_cnt>"
-FILE+="</herbivore>"
-
-#carnivore
-FILE+="<carnivore>"
-FILE+="<path>"
-FILE+=`echo "$PATH"`
-FILE+="/tiger_all_directions.png"
-FILE+="</path>"
-FILE+="<sprite_cnt>4</sprite_cnt>"
-FILE+="</carnivore>"
-
-#unit
-FILE+="<unit>"
-FILE+="<path>"
-FILE+=`echo "$PATH"`
-FILE+="/unit.png"
-FILE+="</path>"
-FILE+="<sprite_cnt>1</sprite_cnt>"
-FILE+="</unit>"
-
+create_node "grass" "1"
+create_node "water" "1"
+create_node "sand" "1"
+create_node "soil" "1"
+create_node "tree" "1"
+create_node "herbivore" "4"
+create_node "carnivore" "4"
+create_node "unit" "1"
+create_node "stone" "1"
 
 FILE+="</evolva>"
 
 cd ..
-echo "$FILE" >> gui.xml
+
+echo -e "$FILE" > gui.xml
