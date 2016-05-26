91f7a098d73fc17528acae005eb6309e259b5aff
Konrad Gotfryd <gotfrydkonrad@gmail.com>
2016-05-26 10:40:55 +0200

    script which downloads sprites works

--- scripts/generate_gui_xml.sh
+++ scripts/generate_gui_xml.sh
@@ -3,10 +3,10 @@
 function create_node() {
 	FILE=`printf "%s\n%s" "$FILE" "<$1>"`
 	FILE=`printf "%s\n%s" "$FILE" "<path>"`
-	FILE=`printf "%s\n%s%s" "$FILE" "$PATH" "/$1.png"`
+	FILE=`printf "%s\n%s%s" "$FILE" "$PATH" "/$1.$2"`
 	FILE=`printf "%s\n%s" "$FILE" "</path>"`
 	FILE=`printf "%s\n%s" "$FILE" "<sprite_cnt>"`
-	FILE=`printf "%s\n%s" "$FILE" "$2"`
+	FILE=`printf "%s\n%s" "$FILE" "$3"`
 	FILE=`printf "%s\n%s" "$FILE" "</sprite_cnt>"`
 	FILE=`printf "%s\n%s\n \n" "$FILE" "</$1>"`
 }
@@ -19,7 +19,7 @@ wget -O grass.png http://cdn.photonesta.com/images/4.bp.blogspot.com/-ggkSFkq3ky
 wget -O sand.png www.wurmpedia.com/images/a/a7/Sand.png
 wget -O soil.png http://www.buildbase.co.uk/img/group/Q/soil.png
 wget -O stone.png http://www.autoglas-specialist.nl/img/stone.png
-wget -O water.png http://www.sharecg.com/images/medium/3020.jpg
+wget -O water.jpg http://www.sharecg.com/images/medium/3020.jpg
 wget -O tree.png http://vignette2.wikia.nocookie.net/herebemonsters/images/4/40/Mango-Tree-Sprite.png/revision/latest?cb=20140328231536
 wget -O unit.png http://www.serwo.pl/images/promo1.png
 
@@ -27,15 +27,15 @@ PATH=`pwd`
 
 FILE="<evolva>"
 
-create_node "grass" "1"
-create_node "water" "1"
-create_node "sand" "1"
-create_node "soil" "1"
-create_node "tree" "1"
-create_node "herbivore" "4"
-create_node "carnivore" "4"
-create_node "unit" "1"
-create_node "stone" "1"
+create_node "grass" "png" "1"
+create_node "water" "jpg" "1"
+create_node "sand" "png" "1"
+create_node "soil" "png" "1"
+create_node "tree" "png" "1"
+create_node "herbivore" "png" "4"
+create_node "carnivore" "png" "4"
+create_node "unit" "png" "1"
+create_node "stone" "png" "1"
 
 FILE+="</evolva>"
 
---
+++ scripts/readme.txt
@@ -0,0 +1,2 @@
+generate_gui_xml.sh creates directory with sprites and gui.xml file in actual 
+	path (from which script generate_gui_xml.sh was called). 
